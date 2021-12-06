2b6b8538b10b58642de71ccc083a10c400c9fe85
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-12-06 10:43:21 +0800

    Add files via upload

---
+++ class08.java
@@ -0,0 +1,11 @@
+
+import pack6.subpack1.subsubpack.CStyle;
+
+public class class08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CStyle sty=new CStyle(1);
+	}
+
+}
