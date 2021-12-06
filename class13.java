48969a2599a76fe68cd8011a497778db37f86300
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-12-06 12:12:09 +0800

    Add files via upload

---
+++ class13.java
@@ -0,0 +1,9 @@
+
+public class class13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("最接近5.38的整數:"+Math.round(5.35));
+	}
+
+}
---
+++ class14.java
@@ -0,0 +1,10 @@
+
+public class class14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Habit is second nature";
+		System.out.println(str.substring(16, str.length()));
+	}
+
+}
